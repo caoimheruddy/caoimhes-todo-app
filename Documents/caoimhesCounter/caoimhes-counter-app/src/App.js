e3ddc3e1b3a079d7d2b977455acc958ce6d4f90c
caoimhe.ruddy <caoimhe.ruddy@travelport.com>
2021-03-01 16:04:43 +0000

    First commit for my counter app

---
+++ Documents/caoimhesCounter/caoimhes-counter-app/src/App.js
@@ -0,0 +1,17 @@
+import Counter from './components/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="headerDiv">
+        <Counter/>
+        </div>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
